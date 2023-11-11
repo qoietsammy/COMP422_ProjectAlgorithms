53065635bbd2fd4d14c753f06d171b91ab70ffbf
Robo-Eye <68978948+Robo-Eye@users.noreply.github.com>
2023-11-11 16:06:01 -0500

    fixed JT file

--- src/JT.java
+++ src/JT.java
@@ -1,37 +1,37 @@
 public class JT {
 
     public static void main(String[] args) {
-        int n = 3;
+        int n = 4;
         SJT(n);
     }
 
-    public static int search(int[] array, int n, int k) {
-        for (int i = 0; i < n - 1; i++) {
-            if (A[n] == k) {
-                return n + 1;
+    public static int search(int[] A, int n, int k) {
+        for (int i = 0; i < n; i++) {
+            if (A[i] == k) {
+                return i + 1;
             }
         }
         return 0;
     }
 
-    public static int findMobile(int[] A, int[] B, int n) {
+    public static int findMobile(int[] A, boolean[] B, int n) {
         int last = 0;
         int mobile = 0;
-        for (int i = 0; i < n - 1; i++) {
-            if (B[A[n] - 1] == false && i != 0) {
+        for (int i = 0; i < n; i++) {
+            if (!B[A[i] - 1] && i != 0) {
                 if (A[i] > A[i - 1] && A[i] > last) {
                     mobile = A[i];
                     last = mobile;
                 }
             }
-            if (B[A[n] - 1] == true && i != n - 1) {
+            if (B[A[i] - 1] && i != n - 1) {
                 if (A[i] > A[i + 1] && A[i] > last) {
                     mobile = A[i];
                     last = mobile;
                 }
             }
         }
-        if (mobile = 0 && last = 0) {
+        if (mobile == 0 && last == 0) {
             return 0;
         } else {
             return mobile;
@@ -39,49 +39,72 @@ public class JT {
     }
 
 
-    public static void printPermutation(int[] A, int[] B, int n) {
-        mobile = findMobile(A, B, n);
-        position = search(A, n, mobile);
-        if (B[A[position - 1] - 1] == false) {
-            int temp = A[position - 1];
-            A[position - 1] = A[position - 2];
-            A[position - 2] = temp;
+    public static void printPermutation(int[] A, boolean[] B, int n) {
+        int mobile = findMobile(A, B, n);
+//        int position = search(A, n, mobile);
+//        if (!B[A[position - 1] - 1]) {
+//            int temp = A[position - 1];
+//            A[position - 1] = A[position - 2];
+//            A[position - 2] = temp;
+//
+//        } else {
+//            int temp = A[position];
+//            A[position] = A[position - 1];
+//            A[position - 1] = temp;
+//        }
+        int position = search(A, n, mobile) - 1;
 
-        } else {
-            int temp = A[position];
-            A[position] = A[position - 1];
-            A[position - 1] = temp;
+        if (position != -1) {
+            if (!B[A[position] - 1]) {
+                int temp = A[position];
+                A[position] = A[position - 1];
+                A[position - 1] = temp;
+            } else {
+                int temp = A[position];
+                A[position] = A[position + 1];
+                A[position + 1] = temp;
+            }
         }
 
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n ; i++) {
             if (A[i] > mobile) {
-                if (B[A[i] - 1] == false) {
+                if (!B[A[i] - 1]) {
                     B[A[i] - 1] = true;
-                } else if (B[A[i] - 1] == true) {
+                } else if (B[A[i] - 1]) {
                     B[A[i] - 1] = false;
                 }
             }
         }
-        for (int i = 0; i < n - 1; i++) {
-            System.out.println(A[i]);
+        for (int i = 0; i < n ; i++) {
+            System.out.print(" "+ A[i] + " ");
         }
+        System.out.println();
     }
 
     public static void SJT(int n) {
-        int A[] = new int[n];
-        int B[] = new int[n];
-        for (int i = 0; i < n - 1; i++) {
+        int[] A = new int[n];
+        boolean[] B = new boolean[n];
+        for (int i = 0; i < n ; i++) {
             A[i] = i + 1;
-            print(A[i]);
+            System.out.print(" "+ A[i] + " ");
         }
-        for (int i = 0; i < n - 1; i++) {
+        System.out.println();
+        for (int i = 0; i < n ; i++) {
             B[i] = false;
         }
-        for (int i = 0; i < n - 1; i++) {
+
+        int temp = factorial(n);
+        for (int i = 0; i < temp ; i++) {
             printPermutation(A, B, n);
         }
 
     }
 
 
+    public static int factorial(int n) {
+        return (n == 1 || n == 0) ? 1 : n * factorial(n - 1);
+    }
 }
+
+
+
